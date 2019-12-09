4ac465fb7c72eda3ba1b84e9bea5d6ba32b2950b
fann <1106862002@qq.com>
2019-12-09 16:08:38 +0800

    可以打包单文件

--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,45 @@
 // vue.config.js
 // const path = require('path');
+let objectProject = {
+  index: {
+    entry: "src/views/index/main.js", // page 的入口
+    template: "public/index.html", // 模板来源
+    filename: "index.html", // 在 dist/index.html 的输出
+    title: "Index Page", // 当使用 title 选项时，template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+    // 在这个页面中包含的块,默认情况下会包含,提取出来的通用 chunk 和 vendor chunk。
+    chunks: ["chunk-vendors", "chunk-common", "index"]
+  },
+  ui: {
+    entry: "src/views/ui/main.js",
+    template: "public/ui.html",
+    filename: "ui.html",
+    chunks: ["chunk-vendors", "chunk-common", "ui"]
+  },
+  page1: {
+    entry: "src/pages/page1/main.js",
+    template: "public/page1.html",
+    filename: "page1.html",
+    chunks: ["chunk-vendors", "chunk-common", "page1"]
+  },
+  page2: {
+    entry: "src/pages/page2/main.js",
+    template: "public/page2.html",
+    filename: "page2.html",
+    chunks: ["chunk-vendors", "chunk-common", "page2"]
+  }
+};
+let page = {};
+let projectname = process.argv[3]; // 获取执行那个文件
+if (process.env.NODE_ENV === "development" || process.env.VUE_APP_ENV === "production") {
+  page = objectProject;
+} else {
+  page[projectname] = objectProject[projectname];
+}
 
 const vueConfig = {
   publicPath: "./", // 官方要求在此修改路径，默认是根目录下，可自行配置
-  outputDir: "dist", // 打包文件
-  pages: {
-    index: {
-      entry: "src/views/index/main.js", // page 的入口
-      template: "public/index.html", // 模板来源
-      filename: "index.html", // 在 dist/index.html 的输出
-      title: "Index Page", // 当使用 title 选项时，template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      // 在这个页面中包含的块,默认情况下会包含,提取出来的通用 chunk 和 vendor chunk。
-      chunks: ["chunk-vendors", "chunk-common", "index"]
-    },
-    ui: {
-      entry: "src/views/ui/main.js",
-      template: "public/ui.html",
-      filename: "ui.html",
-      chunks: ["chunk-vendors", "chunk-common", "ui"]
-    },
-    page1: {
-      entry: "src/pages/page1/main.js",
-      template: "public/page1.html",
-      filename: "page1.html",
-      chunks: ["chunk-vendors", "chunk-common", "page1"]
-    },
-    page2: {
-      entry: "src/pages/page2/main.js",
-      template: "public/page2.html",
-      filename: "page2.html",
-      chunks: ["chunk-vendors", "chunk-common", "page2"]
-    }
-  },
+  outputDir: process.env.VUE_APP_ENV === "production" ? "dist" : "dist" + projectname, // 标识是打包哪个文件
+  pages: page,
   filenameHashing: true,
   productionSourceMap: false, // 生产环境 sourceMap
   devServer: {
